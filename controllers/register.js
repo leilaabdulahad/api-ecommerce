a5de42f5b173cbf65fcfd502d9c5dd477ca25236
Leila <leilaabdulahad@hotmail.com>
2024-03-25 17:20:59 +0100

    Adds hashing

--- controllers/register.js
+++ controllers/register.js
@@ -1,17 +1,20 @@
-const express = require('express');
-const router = express.Router();
-const User = require('../models/User'); // adjust the path according to your project structure
+const express = require('express')
+const router = express.Router()
+const bcrypt = require('bcrypt')
+const User = require('../models/User')
 
 router.post('/', async (req, res) => {
-    const { username, email, password } = req.body;
-    const user = new User({ username, email, password });
+    const { username, email, password } = req.body
     try {
-        await user.save();
-        res.status(201).json({ message: 'User registered successfully' });
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const user = new User({ username, email, password: hashedPassword })
+        await user.save()
+        res.status(201).json({ message: 'User registered successfully' })
     } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Server error' });
+        console.error('Error:', err)
+        res.status(500).json({ error: 'Server error' })
     }
-});
+})
 
-module.exports = router;
+
+module.exports = router
