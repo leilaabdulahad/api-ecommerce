8faf28cc9d057bf67f6bcc7a363cc5c410e851bf
Leila <leilaabdulahad@hotmail.com>
2024-04-11 08:41:24 +0200

    Adds token to registration

--- controllers/register.js
+++ controllers/register.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const bcrypt = require('bcrypt')
 const User = require('../models/User')
+const jwt = require('jsonwebtoken')
+const JWT_SECRET = process.env.JWT_SECRET
 
 router.post('/', async (req, res) => {
     const { firstName, lastName, email, password } = req.body
@@ -10,12 +12,15 @@ router.post('/', async (req, res) => {
         const user = new User({ firstName, lastName, email, password: hashedPassword })
 
         await user.save()
+
+        const payload = { userId: user._id }
+        const token = jwt.sign(payload, JWT_SECRET, { expiresIn: '1h' })
         
-        res.status(201).json({ message: 'User registered successfully' })
+        res.status(201).json({ message: 'User registered successfully', token })
     
     } catch (err) {
         console.error('Error:', err)
-        res.status(500).json({ error: 'Server error' })
+        res.status(500).json({ error: err.message })
     }
 })
 
